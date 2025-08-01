b23207593267be2d3dfd7da8e75c539f3f191ca9
quyet <vuminhquyet18@gmail.com>
2025-08-01 14:24:56 +0700

    abc

---
+++ app/dashboard/login/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import LoginPage from '@/components/pages/login/page'
+export default function LoginPageWrapper() {
+
+    return (
+        <div>
+            <LoginPage />
+        </div>
+    )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import LoginPage from '@/components/pages/login/page'
 export default function DashboardPage() {
 
     return (
         <div>
-            <LoginPage />
+            Đây là dashboard
         </div>
     )
 }
